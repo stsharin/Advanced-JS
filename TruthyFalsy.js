f344f4c7f2152f0bb13acedd3e26987d8b7bc9a3
stsharin <stssharin@gmail.com>
2021-02-02 01:14:38 +0600

    true vs false

---
+++ TruthyFalsy.js
@@ -0,0 +1,20 @@
+// Falsy:
+// false
+// 0
+// undefined
+// null
+// NaN
+//Truthy:
+// 'string', ' ', {}, []
+
+// const fname = "";
+// const age = 0; 
+let fname;
+console.log(fname);
+
+if(fname){
+    console.log("Condition is true");
+}
+else{
+    console.log("condition is false");
+}
