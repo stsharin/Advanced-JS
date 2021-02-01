2d4286b3eea5abf2da426d651f7f1df04b3f0ed4
stsharin <stssharin@gmail.com>
2021-02-02 01:44:32 +0600

    null vs undefined

---
+++ undefined.js
@@ -0,0 +1,14 @@
+// let pakhi;
+function add(num1, num2){
+    console.log(num1+num2);
+    return
+}
+const result = add(13, 12);
+
+const premik = {name: "smart dude", phone: 98200};
+console.log(premik.gf);
+
+let fun = undefined;
+console.log(fun);
+
+// null not exists
